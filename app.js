0dc1787f93654bbc7eb1d813e8682df2b3d61620
Triana Cerda <trianaRcerda@gmail.com>
2021-06-30 15:11:09 -0700

    wrote compareNumbers test

--- app.js
+++ app.js
@@ -1,24 +1,26 @@
 // import functions and grab DOM elements
+const btn = document.getElementById('btn');
 
 // initialize state
-
-// set event listeners 
-//   get user input
-//   use user input to update state 
-//   update DOM to reflect the new state
-
-
-const btn = document.getElementById('btn');
-//INTIAL STATE
 const targetNumber = Math.floor(Math.random() * 20);
 const userGuess = document.getElementById('input');
 const message = document.getElementById('message'); 
 let guessesRemaining = 4;
 
-btn.addEventListener('click', ()=>{
+// set event listeners 
+btn.addEventListener('click', ()=>{  
+  //   get user input
+  //   use user input to update state 
+  //   update DOM to reflect the new state
+    const userGuessNum = Number(userGuess);
+
+
+
 });
+
+
+
 
 //alert('')
 
 //Math.floor(Math.random()*100);
-// dkajhsfkh
--- test/example.test.js
+++ test/example.test.js
@@ -1,6 +1,8 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { compareNumbers } from '../utils.js';
+
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
@@ -16,3 +18,37 @@ test('time to test a function', (expect) => {
     // Make assertions about what is expected versus the actual result
     expect.equal(actual, expected);
 });
+
+test('userGuess is too high', (expect) => {
+   
+    const expected = +1;
+    
+   
+    const actual = compareNumbers(1, 2);
+ 
+    
+    expect.equal(actual, expected);
+});
+
+test('userGuess is equal', (expect) => {
+   
+    const expected = 0;
+    
+   
+    const actual = compareNumbers(1, 1);
+ 
+    
+    expect.equal(actual, expected);
+});
+
+test('userGuess is too low', (expect) => {
+   
+    const expected = -1;
+    
+   
+    const actual = compareNumbers(1, 0);
+ 
+    
+    expect.equal(actual, expected);
+});
+
