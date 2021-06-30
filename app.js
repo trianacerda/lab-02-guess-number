6e7765b31dcdffcd97e233c7448acd5c4b333db9
Triana Cerda <trianaRcerda@gmail.com>
2021-06-30 14:21:58 -0700

    updated guess random number

--- app.js
+++ app.js
@@ -6,3 +6,18 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+
+
+const btn = document.getElementById('btn');
+//INTIAL STATE
+const targetNumber = Math.floor(Math.random() * 20);
+const userGuess = document.getElementById('input');
+const message = document.getElementById('message'); 
+let guessesRemaining = 4;
+
+btn.addEventListener('click', ()=>{
+});
+
+alert('')
+
+//Math.floor(Math.random()*100);
